e61b1ea41051ef83bec5065ac4d0ca843fe09391
MhN_AR <mahan85a@gmail.com>
2026-07-05 23:28:27 +0330

    feat!: add DatabaseConfig Class and DatabaseConnectionException

---
+++ src/ir/library/util/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package ir.library.util;
+
+import ir.library.exception.DatabaseConnectionException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+    private static final String URL = "jdbc:postgresql://localhost5432/hw16";
+    private static final String Username = "postgres";
+    private static final String Password = "2117";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, Username, Password);
+        }
+        catch (SQLException e) {
+            throw new DatabaseConnectionException("Connection Failed!");
+        }
+    }
+}
