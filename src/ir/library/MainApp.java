29a830b9e60eb11a4f66b7a3d10b6e8ddf0a4c09
MhN_AR <mahan85a@gmail.com>
2026-07-07 04:46:37 +0330

    feat: design the menu in console
    Co-authored-by: MhN_AR <mahan85a@gmail.com>

--- src/ir/library/MainApp.java
+++ src/ir/library/MainApp.java
@@ -1,7 +1,33 @@
 package ir.library;
 
+import ir.library.service.BookService;
+import ir.library.service.MemberService;
+
+import java.util.Scanner;
+
 public class MainApp {
     static void main() {
+        MemberService memberService = new MemberService();
+        BookService bookService = new BookService();
 
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("""
+                    ==== Library System Management ====
+                    
+                    1. Insert Member
+                    2. Insert Book
+                    3. Find Member By ID
+                    4. Find Book By ID
+                    5. Find All Members
+                    6. Find All Books
+                    7. Update Member's Full Name
+                    8. Update Book's Price
+                    9. Delete Member
+                    10. Delete Book
+                    0. Exit
+                    """);
+        }
     }
 }
